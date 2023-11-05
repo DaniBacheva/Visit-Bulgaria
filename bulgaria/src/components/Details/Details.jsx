849e6dbb1a393bf78abbc000b207fca49f0033b9
DaniBacheva <y.bacheva@abv.bg>
2023-11-05 21:44:26 +0200

    Add details to place

--- bulgaria/src/components/Details/Details.jsx
+++ bulgaria/src/components/Details/Details.jsx
@@ -1,30 +1,36 @@
-import {useParams} from 'react-router-dom'
+import {useParams} from 'react-router-dom';
+import * as placeService from '../../services/placeService'
+import { useEffect, useState } from 'react';
 
 export default function Details() {
-  const {placeId} = useParams()
+
+  const {placeId} = useParams();
+  const [place, setPlace] = useState({})
+
+  useEffect(()=> {
+    placeService.getOne(placeId)
+    .then(result=> {
+      setPlace(result)
+    })
+  },[placeId]);
+
+
   return (
     <section id="details">
       <div id="details-wrapper">
         <div className="basic">
-          <p id="details-name">Rila Monastery</p>
-          <img id="details-img" src="https://cdn.thecrazytourist.com/wp-content/uploads/2023/01/ccimage-shutterstock_1160109367.jpg" />
-          <p id="location">Location: 80 km FROM SOFIA</p>
+          <p id="details-name">{place.name}</p>
+          <img id="details-img" src={place.imageUrl} />
+          <p id="location">Location: {`${place.location}`}</p>
         </div>
         <div id="info-wrapper">
           <div id="details-description">
 
             <p id="description">
-              Perhaps the most famous Eastern Orthodox monastery in the world,
-              Rila has risen and risen to become a veritable symbol of the Bulgarian nation.
+              {place.description}
             </p>
             <p id="more-info">
-              It entered the UNESCO World Heritage List way back in 1983, hailed for its curious
-              intermingling of Mamluk, arabesque, Byzantine and Romanesque styles, and resplendent
-              iconostases walls, carved meticulously and inlaid with shimmering gold leaf.
-
-              An on-site museums helps travelers unravel the more than 1,000 years of history that
-              coalesce at the site, while endless courtyards and peristyles decorated in murals and medieval
-              scenes mean there’s plenty of art and architecture to draw the eye.
+          {place['additional-info']}
             </p>
           </div>
         </div>
--- bulgaria/src/services/placeService.js
+++ bulgaria/src/services/placeService.js
@@ -2,18 +2,25 @@ import * as request from './request'
 
 const baseUrl = 'http://localhost:3030/jsonstore/places'
 
-export const getAll= async ( ) => {
-    const result = await request.get (baseUrl);
-    const places = Object.values(result)
+export const getAll = async () => {
+  const result = await request.get(baseUrl);
+  const places = Object.values(result)
 
   console.log(places);
-  
-    return places;
+
+  return places;
 }
 
 
-export const create =async (placeData) => {
+export const create = async (placeData) => {
   const result = await request.post(baseUrl, placeData);
+
+  console.log(result);
+  return result;
+}
+
+export const getOne = async (placeId) => {
+  const result = await request.get(`${baseUrl}/${placeId}`);
 
   console.log(result);
   return result;
